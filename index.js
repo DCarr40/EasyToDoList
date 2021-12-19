c3a81f9791a7e374d55906dd8d7b09e5dc4efefd
DCarr40 <davidwcarruthers@yahoo.com>
2021-12-18 21:23:09 -0500

    added 3 variables and a function to add input to page

---
+++ index.js
@@ -0,0 +1,8 @@
+let addTodoButton = document.getElementById("addTodo");
+let listTodos = document.getElementById("todos");
+let inputField = document.getElementById("inputField");
+
+addTodoButton.addEventListener("click", () => {
+  let para = document.createElement("p");
+  todos.appendChild(para);
+});
